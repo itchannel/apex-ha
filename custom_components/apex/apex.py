9451c3a70d3a270abb23677ef9601266df7fcfc8
Bretton Wade <bretton.wade@gmail.com>
2022-09-24 00:29:31 -0400

    main - turn the DOS off first...

--- custom_components/apex/apex.py
+++ custom_components/apex/apex.py
@@ -101,47 +101,48 @@ class Apex(object):
             logger.error(f"Profile index mismatch (expected {profile_id}, got {profile['ID']}")
             return None
 
-        # check if the requested rate is greater than the OFF threshold
-        min_rate = 0.1
-        if rate > min_rate:
-            # our input is a target rate (ml/min). we want to map this to the nearest 0.1ml/min, and
-            # then find the slowest pump speed possible to manage sound levels. Neptune uses a 3x
-            # safety margin to extend the life of the pump, but the setting only appears to be
-            # enforced in the Fusion UI. We use a 2x margin because we can.
-            pump_speeds = [250, 125, 60, 25, 12, 7]
-            safety_margin = 2
-            rate = int(rate * 10) / 10.0
+        # turn the pump off to start - this will enable a new profile setting to start immediately
+        # without it, the DOS will wait until the current profile period expires
+        if self.set_variable(did, "Set OFF") is not None:
+            # check if the requested rate is greater than the OFF threshold
+            min_rate = 0.1
+            if rate > min_rate:
+                # our input is a target rate (ml/min). we want to map this to the nearest 0.1ml/min, and
+                # then find the slowest pump speed possible to manage sound levels. Neptune uses a 3x
+                # safety margin to extend the life of the pump, but the setting only appears to be
+                # enforced in the Fusion UI. We use a 2x margin because we can.
+                pump_speeds = [250, 125, 60, 25, 12, 7]
+                safety_margin = 2
+                rate = int(rate * 10) / 10.0
 
-            # if the requested rate is within the supported range
-            if int(pump_speeds[0] / safety_margin) >= rate:
-                # find the best speed match
-                target_pump_speed = rate * safety_margin
-                pump_speed_index = len(pump_speeds) - 1
-                while pump_speeds[pump_speed_index] < target_pump_speed:
-                    pump_speed_index -= 1
+                # if the requested rate is within the supported range
+                if int(pump_speeds[0] / safety_margin) >= rate:
+                    # find the best speed match
+                    target_pump_speed = rate * safety_margin
+                    pump_speed_index = len(pump_speeds) - 1
+                    while pump_speeds[pump_speed_index] < target_pump_speed:
+                        pump_speed_index -= 1
 
-                # bits 0-4 of the 'mode' value are the pump speed index, and bit 5 specifies
-                # 'forward' or 'reverse'. we always use 'forward' because you can't calibrate the
-                # reverse direction using the Apex dashboard
-                mode = pump_speed_index + 16
+                    # bits 0-4 of the 'mode' value are the pump speed index, and bit 5 specifies
+                    # 'forward' or 'reverse'. we always use 'forward' because you can't calibrate the
+                    # reverse direction using the Apex dashboard
+                    mode = pump_speed_index + 16
 
-                # we set the profile to be what we need it to be so the user doesn't have to do
-                # anything except choose the profile to use
-                profile["type"] = "dose"
-                profile_name = profile["name"] = f"Dose_{did}"
+                    # we set the profile to be what we need it to be so the user doesn't have to do
+                    # anything except choose the profile to use
+                    profile["type"] = "dose"
+                    profile_name = profile["name"] = f"Dose_{did}"
 
-                # the DOS profile is the mode, target amount, target time period (one minute), and
-                # dose count
-                profile["data"] = {"mode": mode, "amount": rate, "time": 60, "count": 255}
+                    # the DOS profile is the mode, target amount, target time period (one minute), and
+                    # dose count
+                    profile["data"] = {"mode": mode, "amount": rate, "time": 60, "count": 255}
 
-                # turn the pump off to start - this will enable a new profile setting to start immediately
-                # without it, the DOS will wait until the current profile period expires
-                if self.set_variable(did, "Set OFF") is not None:
+                    # try to set the profile data, and if successful set the pump to it
                     if self.try3(f"http://{self.deviceip}/rest/config/pconf/{profile_id}", postdata=profile) is not None:
                         return self.set_variable(did, f"Set {profile_name}")
+                else:
+                    logger.error(f"Requested rate ({rate} mL / min) exceeds the supported range (limit {int(pump_speeds[0] / safety_margin)} mL / min).")
             else:
-                logger.error(f"Requested rate ({rate} mL / min) exceeds the supported range (limit {int(pump_speeds[0] / safety_margin)} mL / min).")
-        else:
-            # XXX TODO handle 0 < rate < 0.1ml/min by dosing over multiple minutes? Is this necessary?
-            logger.warning(f"dosing does not currently support < {min_rate} mL / min")
+                # XXX TODO handle 0 < rate < 0.1ml/min by dosing over multiple minutes? Is this necessary?
+                logger.warning(f"dosing does not currently support < {min_rate} mL / min")
         return None
