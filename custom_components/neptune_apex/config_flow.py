953d7f90923417643808989bb0a69d3052fbe277
Bretton Wade <bretton.wade@gmail.com>
2024-07-31 19:03:07 -0400

    zeroconf - updating config flow handler

--- custom_components/neptune_apex/config_flow.py
+++ custom_components/neptune_apex/config_flow.py
@@ -1,4 +1,6 @@
 import logging
+from asyncio import Task
+from typing import Any, Dict, Coroutine, Mapping, Container
 
 import voluptuous as vol
 from homeassistant import config_entries, core, exceptions
@@ -6,6 +8,9 @@ from homeassistant.components.zeroconf import ZeroconfServiceInfo
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import callback
+from homeassistant.data_entry_flow import FlowResultType
+from voluptuous import Schema
+
 from .const import SYSTEM, HOSTNAME
 
 from .const import (
@@ -17,13 +22,6 @@ from .const import (
 from .apex import Apex
 
 logger = logging.getLogger(__name__)
-
-DATA_SCHEMA = vol.Schema({
-        vol.Required(CONF_USERNAME): str,
-        vol.Required(CONF_PASSWORD): str,
-        vol.Required(DEVICEIP): str,
-    })
-
 
 async def validate_input(hass: core.HomeAssistant, data):
     apex = Apex(data[CONF_USERNAME], data[CONF_PASSWORD], data[DEVICEIP])
@@ -63,21 +61,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 logger.exception(f"Unexpected exception {exc}")
                 errors["base"] = "unknown"
 
+        data_schema = vol.Schema({
+            vol.Required(CONF_USERNAME): str,
+            vol.Required(CONF_PASSWORD): str,
+            vol.Required(DEVICEIP, default=None if user_input is None else user_input.get(DEVICEIP, None)): str,
+        })
+
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=data_schema, errors=errors
         )
 
-    async def async_step_zeroconf(self, discovery_info: ZeroconfServiceInfo) -> ConfigFlowResult:
+    async def async_step_zeroconf(self, discovery_info: ZeroconfServiceInfo):
         # use the serial number as a unique identifier
         await self.async_set_unique_id(discovery_info.properties["sn"])
         self._abort_if_unique_id_configured()
         logger.debug(f"zeroconf discovered (device: {discovery_info.properties["sn"]}, hostname: {discovery_info.properties["hn"]}, ip_address: {discovery_info.ip_address})")
-        data_schema = vol.Schema({
-            vol.Required(CONF_USERNAME): str,
-            vol.Required(CONF_PASSWORD): str,
-            vol.Required(DEVICEIP, default=discovery_info.ip_address): str,
-        })
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors={})
+        return await self.async_step_user(user_input={DEVICEIP: discovery_info.ip_address})
 
     @staticmethod
     @callback
