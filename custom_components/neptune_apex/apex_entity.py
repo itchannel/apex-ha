1a65bed56d58554a7a86631697a11ee8916cd93c
Bretton Wade <bretton.wade@gmail.com>
2024-08-01 02:14:35 -0400

    zeroconf - converted dashes in the names.

--- custom_components/neptune_apex/apex_entity.py
+++ custom_components/neptune_apex/apex_entity.py
@@ -12,7 +12,7 @@ class ApexEntity(CoordinatorEntity):
         super().__init__(coordinator)
 
         # we do not pass a name up the tree
-        self._device_id = self._attr_unique_id = f"{coordinator.hostname}_{entity[NAME]}".lower()
+        self._device_id = self._attr_unique_id = f"{coordinator.hostname}_{entity[NAME]}".lower().replace("-", "_")
         self._attr_name = f"{coordinator.hostname.capitalize()} {entity[NAME]}"
         logger.debug(f"{entity_type}.{self._device_id} = (NAME: {entity[NAME]}, DID: {entity[DID]}, TYPE: {entity[TYPE]})")
 
